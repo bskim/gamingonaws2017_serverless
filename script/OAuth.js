896b90278a6e169e3212dcc21c7f803e83e1a0a8
bskim <metamorp@gmail.com>
2018-06-20 18:20:19 +0900

    typo

--- script/OAuth.js
+++ script/OAuth.js
@@ -2,5 +2,5 @@ var CONFIG = {
     "KakaoAppId":"<Kakao Javascript AppKey>",
     "API_URL":" API Gateway prod enpoint URL(+resource path if not root) ",
     "fbAppId":"<Facebook App ID>",
-    "googleAPIclientID":"<Google Client id (including apps.googleusercontent.com)>
+    "googleAPIclientID":"<Google Client id (including apps.googleusercontent.com)>"
 }
